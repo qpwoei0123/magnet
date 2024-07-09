f970b4e7bec90b57471a96b8dba4ee2b4a228d54
qpwoei0123 <qpwoei0123@naver.com>
2024-07-09 19:40:42 +0900

    refactor: 경고 문구 컴포넌트 리렌더 개선

--- client/src/component/common/WarningMessage.tsx
+++ client/src/component/common/WarningMessage.tsx
@@ -1,8 +1,10 @@
+import {memo} from 'react';
+
 type WarningMessageProps = {
 	message: string;
 	isSuccess?: boolean;
 };
 
-export const WarningMessage = ({message, isSuccess = false}: WarningMessageProps) => {
+export const WarningMessage = memo(({message, isSuccess = false}: WarningMessageProps) => {
 	return isSuccess ? null : <p className={`${'warning animate-shake'} my-1`}>{message}</p>;
-};
+});
