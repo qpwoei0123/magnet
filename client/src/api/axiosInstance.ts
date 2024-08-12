ddea6f4492ab5e10a5372ab35dd679910a36e22a
qpwoei0123 <qpwoei0123@naver.com>
2024-08-12 14:59:19 +0900

    feat: 인증 인스턴스 추가

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const baseUrl = process.env.REACT_APP_BASE_URL || 'NO_BASE_URL';
 
+// 기본 인스턴스 (토큰 없이 사용할 수 있는 요청)
 export const axiosInstance = axios.create({
 	baseURL: baseUrl,
 	headers: {
@@ -9,18 +10,28 @@ export const axiosInstance = axios.create({
 	},
 });
 
-// 요청 인터셉터를 통해 토큰이 존재하면 추가
-axiosInstance.interceptors.request.use(
+// 토큰 전용 인스턴스 (토큰이 필요한 요청 전용)
+export const axiosInstanceWithAuth = axios.create({
+	baseURL: baseUrl,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
+
+axiosInstanceWithAuth.interceptors.request.use(
+	// 요청 인터셉터를 통해 토큰 추가
 	config => {
 		const authorToken = sessionStorage.getItem('Authorization');
 		const refreshToken = sessionStorage.getItem('RefreshToken');
 
-		if (authorToken) {
-			config.headers['Authorization'] = authorToken;
+		if (!authorToken && !refreshToken) {
+			return Promise.reject(
+				new Error('Authorization 토큰과 RefreshToken 토큰이 모두 존재하지 않습니다.'),
+			);
 		}
-		if (refreshToken) {
-			config.headers['RefreshToken'] = refreshToken;
-		}
+
+		config.headers['Authorization'] = authorToken;
+		config.headers['RefreshToken'] = refreshToken;
 
 		return config;
 	},
