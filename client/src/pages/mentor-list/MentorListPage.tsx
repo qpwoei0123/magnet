15829d404890fe0ffb646c4c9da2438e177e2448
qpwoei0123 <qpwoei0123@naver.com>
2024-08-13 11:29:48 +0900

    refactor: MentorListPage 폴더화

--- client/src/pages/mentor-list/MentorListPage.tsx
+++ client/src/pages/mentor-list/MentorListPage.tsx
@@ -1,17 +1,24 @@
 import {useEffect, useState} from 'react';
-import MentorCard from '../component/MentorCard';
-import {LoadingContainer} from '../component/common/LoadingContainer';
-import {getMentoringList} from '../api/mentoring';
-import {Content} from '../types/api/mentoring';
-import {categories} from '../asset/categories';
+import {useLocation} from 'react-router-dom';
+import MentorCard from '../../component/MentorCard';
+import {LoadingContainer} from '../../component/common/LoadingContainer';
+import {getMentoringList} from '../../api/mentoring';
+import {Content} from '../../types/api/mentoring';
+import {categories} from '../../asset/categories';
+import Pagination from './Pagination';
 
 const MentoringListPage = () => {
 	const [loading, setLoading] = useState(true);
 	const [category, setCategory] = useState('ALL');
 	const [mentoringList, setMentoringList] = useState<Content[]>([]);
-	const [currentPage, setCurrentPage] = useState(1);
 	const [totalPages, setTotalPages] = useState(0);
 	const [size] = useState(10);
+
+	const location = useLocation();
+
+	// 현재 페이지를 URL 파라미터로부터 가져옴
+	const currentPage = Number(new URLSearchParams(location.search).get('page') || 1);
+
 	const newCategories = [{id: 'ALL', title: '전체', icon: 'menu-line'}].concat(categories);
 	const filteredMentoringList = mentoringList.filter(
 		el => el.category === category || category === 'ALL',
@@ -33,14 +40,6 @@ const MentoringListPage = () => {
 		fetchMentoringList();
 	}, [currentPage, size]);
 
-	const handlePreviousPage = () => {
-		setCurrentPage(prevPage => prevPage - 1);
-	};
-
-	const handleNextPage = () => {
-		setCurrentPage(prevPage => prevPage + 1);
-	};
-
 	return (
 		<section className="flexCol rootPageSection items-center gap-10 py-10 sm:px-10">
 			<div className="flexCenter animate-fadeInMoveDown flex-wrap">
@@ -57,22 +56,9 @@ const MentoringListPage = () => {
 					</button>
 				))}
 			</div>
-			{/* <div className="textBase flexCenter mt-4 w-full justify-end px-10">
-				<select
-					value={size}
-					onChange={e => setSize(parseInt(e.target.value))}
-					className="rounded-md border p-1 outline-none"
-				>
-					<option value="3">3</option>
-					<option value="6">6</option>
-					<option value="9">9</option>
-				</select>
-				개 씩 보기
-			</div> */}
 			{loading ? (
 				<LoadingContainer />
 			) : filteredMentoringList.length === 0 ? (
-				// 비어있을 경우
 				<div className="flexCol mt-20  items-center gap-2 text-secondary">
 					<i className="ri-ghost-line ri-2x animate-pulse "></i>
 					<p className="textBase animate-fadeIn transition-opacity delay-300">
@@ -86,25 +72,7 @@ const MentoringListPage = () => {
 							<MentorCard key={index} mentoring={el} />
 						))}
 					</div>
-					<div className="flexCenter textSmall mt-4">
-						<button
-							onClick={handlePreviousPage}
-							disabled={currentPage === 1}
-							className="activeStyle border font-semibold disabled:hover:text-slate-400"
-						>
-							이전
-						</button>
-						<p className="textBase px-4 py-2 font-semibold text-gray-700">
-							{currentPage} / {totalPages}
-						</p>
-						<button
-							onClick={handleNextPage}
-							disabled={currentPage === totalPages}
-							className={`activeStyle border font-semibold disabled:hover:text-slate-400`}
-						>
-							다음
-						</button>
-					</div>
+					<Pagination currentPage={Number(currentPage)} totalPages={totalPages} />
 				</>
 			)}
 		</section>
